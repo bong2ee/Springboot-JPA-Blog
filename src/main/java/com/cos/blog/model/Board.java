816511d624ea3ea61d3c9809daec28c12726548b
bong2ee <qhduddlfkda@gmail.com>
2023-04-05 00:52:53 +0900

    회원가입 Insert Test

--- src/main/java/com/cos/blog/model/Board.java
+++ src/main/java/com/cos/blog/model/Board.java
@@ -1,15 +1,18 @@
 package com.cos.blog.model;
 
 import java.sql.Timestamp;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
@@ -39,9 +42,13 @@ public class Board {
 	@ColumnDefault("0")
 	private int count; // 조회수
 	
-	@ManyToOne // 한 명의 유저는 여러 개의 게시글을 가질 수 있다
+	@ManyToOne(fetch = FetchType.EAGER) // 한 명의 유저는 여러 개의 게시글을 가질 수 있다
 	@JoinColumn(name = "userId")
 	private User user; // DB는 오브젝트를 저장할 수 없다. FK, Java는 오브젝트를 저장할 수 있다
+	
+	// mappedBy 연관관계의 주인X , 컬럼을 만들지 마세요 (FK가 아니에요)
+	@OneToMany(mappedBy = "board", fetch = FetchType.EAGER) // reply 테이블에 있는 board
+	private List<Reply> reply; // 하나의 게시글은 여러개의 댓글을 가질 수 있다
 	
 	@CreationTimestamp //시간 자동 입력
 	private Timestamp createDate;
--- src/main/java/com/cos/blog/model/User.java
+++ src/main/java/com/cos/blog/model/User.java
@@ -4,12 +4,15 @@ import java.sql.Timestamp;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.DynamicInsert;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -22,6 +25,7 @@ import lombok.NoArgsConstructor;
 @Builder // 빌더 패턴 !!
 // ORM -> Java(다른언어) Object -> 테이블로 매핑해주는 기술
 @Entity //User 클래스가 MySQL에 테이블로 생성된다
+//@DynamicInsert // insert 할 때 null인 필드 제외
 public class User {
 
 	@Id //Primary key
@@ -37,8 +41,10 @@ public class User {
 	@Column(nullable = false, length = 50) // null X, 길이 50자 제한
 	private String email;
 	
-	@ColumnDefault("'user'")
-	private String roll; // Enum을 쓰는게 좋다 // admin, user, manager
+	//@ColumnDefault("'user'")
+	//DB는 RoleType이라는 게 없다
+	@Enumerated(EnumType.STRING)
+	private RoleType roll; // Enum을 쓰는게 좋다 // admin, user, manager
 	   
 	@CreationTimestamp // 시간이 자동으로 입력
 	private Timestamp createDate;
