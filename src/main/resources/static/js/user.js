a9218784eb57a667d8a03d1958b43a02fd393d29
bong2ee <qhduddlfkda@gmail.com>
2023-04-11 22:57:39 +0900

    회원가입 Ajax 요청 1

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -1,6 +1,6 @@
 let index = {
         init:function() {
-            $("#btn-save").on("click",()=>{
+            $("#btn-save").on("click",()=>{ // function(){}, ()=>{} this를 바인딩하기 위해서!!
                 this.save();
             });
         },
@@ -10,10 +10,28 @@ let index = {
             let data = {
                 username: $("#username").val(),
                 password: $("#password").val(),
-                email: $("#password").val()
-            }
+                email: $("#email").val()
+            };
 
-            console.log(data);
+            //console.log(data);
+
+            //ajax 호출시 default가 비동기 호출
+            //ajax 통신을 이용해서 3개의 데이터를 json으로 변경해 insert 요청!!
+            //ajax가 통신을 성공하고 서버가 json을 리턴해주면 자동으로 자바 오브젝트로 변환해주네요
+            $.ajax({
+                type:"POST",
+                url:"/blog/api/user", // /join은 생략 -> POST insert 인식
+                data:JSON.stringify(data), //http body 데이터
+                contentType:"application/json; charset=utf-8", //body 데이터가 어떤 타입인지(MIME)
+                dataType:"json" //요청을 서버로 해서 응답이 왔을 때 기본적으로 모든 것이 문자열(생긴게 json이라면) => javascript 오브젝트로 변경
+            }).done(function(resp){
+                alert("회원가입이 완료되었습니다.");
+                //alert(resp);
+                //console.log(resp);
+                location.href="/blog";
+            }).fail(function(error){
+                alert(JSON.stringify(error));
+            }); 
     }
 }
 
